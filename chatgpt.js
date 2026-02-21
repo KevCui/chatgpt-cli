a1097eaf072d2eef6e73bc7f61c8af6a1ab7ef74
Kevin Cui <krazycavin@gmail.com>
2026-02-21 22:43:01 +0100

    Simplify the code

--- chatgpt.js
+++ chatgpt.js
@@ -5,48 +5,32 @@ const stealth = require('puppeteer-extra-plugin-stealth')();
 chromium.use(stealth);
 
 const searchText = process.argv[2];
-const turnSearchOn = process.argv[3];
-const url = 'https://chatgpt.com';
+const url = 'https://chatgpt.com/?hints=search';
 const buttonSubmit = '[data-testid="send-button"]';
-const buttonSearch= '[aria-label="Search"]';
+const buttonStop = '[data-testid="stop-button"]';
 const textareaSearchBox = '#prompt-textarea';
 const textMessage = '.markdown';
-const totalLoopCount = 60;
-const printIntervalTime = 1000;
 
-chromium.launch({ headless: false, timeout: 30000 }).then(async browser => {
+chromium.launch({ headless: true, timeout: 30000 }).then(async browser => {
   // Set page 
-  const context = await browser.newContext({
-    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
-  });
+  const context = await browser.newContext({});
   const page = await context.newPage();
-  await page.setExtraHTTPHeaders({
-    'Accept-Language': 'en-US,en;q=0.9'
-  });
 
   // Start page
   await page.goto(url, { waitUntil: 'domcontentloaded' });
 
-  // Enable Search
-  if (turnSearchOn) {
-    await page.click(buttonSearch);
-  }
-    
   // Submit question
   await page.fill(textareaSearchBox, searchText);
   await page.click(buttonSubmit);
 
   // Get reply
-  var prevResult = '';
-  for (let i = 0; i < totalLoopCount; i++) {
-    await new Promise((resolve) => setTimeout(resolve, printIntervalTime));
-    const result = await page.locator(textMessage).innerText();
+  await page.waitForTimeout(1000); 
+  const stop = page.locator(buttonStop);
+  while (await stop.count() > 0) {
+    const result = await page.locator(textMessage).allInnerTexts();
+    const resultStr = result.toString();
     console.clear();
-    console.log('----------\n' + result.replace(/^\s*\n+/gm, '\n'));
-    if (prevResult.length > 1 && prevResult == result && i != (totalLoopCount - 1)) {
-      i = (totalLoopCount - 1);
-    }
-    prevResult = result
+    console.log('----------\n' + resultStr);
   }
 
   // Close browser
